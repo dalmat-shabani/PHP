420d1e4593bd47eff1ccac6f1a5287470f7952d9
dalmat-shabani <dalmat.shabani90@gmail.com>
2024-10-10 19:08:17 +0200

    Initial commit

---
+++ Module2/index.php
@@ -0,0 +1,21 @@
+<?php
+//$school = 'Digital School';
+//echo "I love $school";
+
+//int , float , string, boolean, object , recourses , null
+
+//$x = 120;
+//$y = 50;
+//echo $x + $y;
+//echo $x - $y;
+//echo $x * $y;
+//echo $x / $y;
+
+//string
+$a = 'Digital School';
+$b = 'School';
+
+$c = $a.$b;
+echo "$c";
+
+?>
