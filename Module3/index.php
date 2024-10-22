bc0145c00532ed075eb5bef6448f9b17c4895f51
dalmat-shabani <dalmat.shabani90@gmail.com>
2024-10-22 18:31:17 +0200

    a

--- Module3/index.php
+++ Module3/index.php
@@ -70,4 +70,35 @@ switch($grade){
     break;
 }
 
+
+for($x = 0; $x <=10; $x++){
+    echo "The number is $x <br>";
+};
+
+
+$X = 1;
+do {
+    echo "The number is $x<br>";
+    $X++;
+}while ($x <= 5);
+
+
+$x = 1;
+while($x <= 5) {
+    echo "The number is $x<br>";
+    $x++;
+};
+
+
+$car = array("BMW","VW","Audi","Tesla");
+    foreach($car as $value) {
+        echo "$value<br>";
+    };
+
+
+$age = array("John"=>"18", "Michael"=>"23", "Joe"=>"10");
+    foreach($age as $x => $val){
+        echo "$x=$val<br>";
+    }
+
 ?>
