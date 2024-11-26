8670311ff4f86b16d920e2f713f583af50f45bcb
dalmat-shabani <dalmat.shabani90@gmail.com>
2024-11-26 19:15:50 +0100

    a

---
+++ Module10/index.php
@@ -0,0 +1,23 @@
+<?php
+require_once 'config.php';
+
+try{
+    $sql = "SELECT id, username, email, password FROM users";
+    $stmt = $pdo->query($sql);
+    $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
+}catch(PDOException $e){
+    echo "Error fetching data: ".$e->getMessage();
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html> 
